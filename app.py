9834d457bafef3bb582847b4323e0e7a0348d133
Viswanath.Barenkala <code.vissu@gmail.com>
2019-10-20 16:57:37 +0530

    server related changes

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 from flask import Flask, url_for, render_template, request
-from flask_mysqldb import MySQL
+#from flask_mysqldb import MySQL
 #from flask_ngrok import run_with_ngrok
 import random
+import pyodbc
 
 app = Flask(__name__)
 #run_with_ngrok(app)
 
+'''
+app.config['MYSQL_HOST'] = 'deepdivedb.database.windows.net'
+app.config['MYSQL_USER'] = 'deepdive'
+app.config['MYSQL_PASSWORD'] = 'admin@123'
+app.config['MYSQL_DB'] = 'deepdivedb'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'cancerdetection'
-
-mysql = MySQL(app)
-
+server_name='deepdivedb.database.windows.net'
+db_name='deepdivedb'
+#mysql = MySQL(app)
+connection = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+	"Server=server_name;"
+	"Database=db_name;"
+	"uid=deepdive;pwd=admin@123")# Creating Cursor  
+#connection = pyodbc.connect('Driver={SQL Server};''Server=server_name;''Database=db_name;''Trusted_Connection=yes;')# Creating Cursor  
 #cursor = mysql.connection.cursor()
+'''
+server = 'deepdivedb.database.windows.net'
+database = 'deepdivedb'
+username = 'deepdive'
+password = 'admin@123'
+driver= '{ODBC Driver 17 for SQL Server}'
+connection = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
 @app.route('/')
 def api_root():
@@ -28,7 +42,7 @@ def api_articles():
 def api_article():
 	inputInfo = request.form['pId']
 	print(inputInfo)
-	return writetodb(textSummar(),randomInfo(),inputInfo)
+	return writetodb(textSummary(),randomInfo(),inputInfo)
 
 
 def randomInfo():
@@ -38,11 +52,11 @@ def randomInfo():
     randstring = randstring + str(random.randint(1,101))
     return randstring
 
-def textSummar():
+def textSummary():
 	return ("This is newsample text" + str(random.randint(1,101)))
 
 def getDbData(articleid):
-    cur = mysql.connection.cursor()
+    cur = connection.cursor()
     #cur.execute("select pName from PatientInfo where pId=1",)
     sql_select_query = """select pName from PatientInfo where pId = %s"""
     cur.execute(sql_select_query, (articleid,))
@@ -56,28 +70,29 @@ def getDbData(articleid):
     return record[0][0]
 
 def writetodb(summary,yaxisdata,patientid):
-	cursor = mysql.connection.cursor()
+	cursor = connection.cursor()
 	# Prepare SQL query to INSERT a record into the database.
 	#sql = "UPDATE patientInfo SET pSummary=(?) WHERE pId = 20",('this is sample text')
 	try:
 	   # Execute the SQL command
 	   print("Executing the command")
-	   sql_select_query = """UPDATE PatientInfo SET pSummary=%s , pGraphaYaxis=%s WHERE pId = %s"""
+	   #sql_select_query = """UPDATE PatientInfo SET pSummary=%s , pGraphaYaxis=%s WHERE pId = %s"""
+	   cursor.execute("UPDATE patientInfo SET pSummary=(?), pGraphYaxis=(?) WHERE pId = (?)",(summary,yaxisdata,patientid))
 	   #cursor.execute("UPDATE PatientInfo SET pSummary=(?) , pGraphaYaxis=(?) WHERE pId = 1",(summary,yaxisdata,patientid))
-	   cursor.execute(sql_select_query, (summary,yaxisdata,patientid))
+	   #cursor.execute(sql_select_query, (summary,yaxisdata,patientid))
 	   #cursor.execute("UPDATE PatientInfo SET pSummary='lsdjflsjdfljsaldfjsadfljas' WHERE pId = 1")
 	   # Commit your changes in the database
 	   print("Commiting the database")
-	   mysql.connection.commit()
-	   print("Closing the db connection")
-	   cursor.close()	   
+	   connection.commit()
+	   #print("Closing the db connection")
+	   #cursor.close()	   
 	   return "SUCCESS"
 	except:
 	   # Rollback in case there is any error
 	   print("Rolling back due to error")
-	   mysql.connection.rollback()
-	   print("Closing the db connection")
-	   cursor.close()	   
+	   connection.rollback()
+	   #print("Closing the db connection")
+	   #cursor.close()	   
 	   return "FAILURE"
 
 
